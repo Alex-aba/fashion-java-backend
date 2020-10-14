1b479e37a7806815cdff7e6cc815dbd07308e785
Oleksandr Bartkov <consigliere.aba@gmail.com>
2020-10-15 01:39:13 +0300

    Initial commit

---
+++ src/main/java/com/fashion/fashionjavabackend/FashionJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fashion.fashionjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionJavaBackendApplication.class, args);
+	}
+
+}
